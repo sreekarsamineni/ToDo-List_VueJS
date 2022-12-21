c4284eaba97ecfe41dabd40688a4cb472da1f501
sreekarsamineni <saminenisreekarchowdary@gmail.com>
2022-12-21 15:57:03 +0530

    updated dsn and dialog box

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,54 +1,214 @@
 <template>
 
-    <v-container class="con">
-        <v-row>
-            <v-col>
-    <v-card 
-      class="mx-auto my-12"
-      max-width="100%"
-      height="600px"
-      color="#ffffff4d"
-    >
-      <v-card-title>To do List</v-card-title>
+<v-container class="con">
+   <v-row>
+        <v-col>
+            <v-card 
+            class="mx-auto my-12"
+            max-width="75%"
+            height="600px"
+            color="#ffffff4d"
+            >
 
-      
+            <v-card-title>To do List</v-card-title>
+
+
   <!-- Inner card 1 -->
-    <v-card 
-      class="box1 my-12"
-      max-width="100%"
-      height="80px"
-      color="#ffffff4d"
-    >
+            <v-card
+            class="box1"     
+            max-width="100%"
+            height="80px"
+            color="#ffffff4d"
+            >
       
-
-      <div class="add-task">
-      <input type="text" placeholder="Add New Task" class="task-input">
-      <input type="" value="" class="submit-task" title="Add Task">
-    </div>
+            <div class="add-task">
+                <input type="text" placeholder="Add New Task" class="task-input">
+                <!-- <input @click="submitTask" type="" value="" class="submit-task" title="Add Task"> -->
 
 
+     <!-- Create Dialog Box             -->
+                <v-row justify="center">
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="600px"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+        class="btt"
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+          @click="dialog = true"
+        >
+          Add Task
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">Create Task</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  v-model="name"
+                  :rules="nameRules"
+                  label="Name*"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                v-model="aname"
+                  :rules="anameRules"
+                  label="Assignee name*"
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                v-model="date"
+                  :rules="dateRules"
+                  label="Due date*"
+                  hint="Format: dd/mm/yy"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                v-model="email"
+                  :rules="emailRules"
+                  label="Email*"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                v-model="sum"
+                  :rules="sumRules"
+                  label="Summary*"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                v-model="des"
+                  :rules="desRules"
+                  label="Description*"
+                  required
+                ></v-text-field>
+              </v-col>
+              
+              
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+          <v-btn
+          
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
       </v-card>
+    </v-dialog>
+  </v-row>
+
+            </div>
+
+            </v-card>
+
+      <!-- <v-card-text>Hello</v-card-text> -->
 
 
     <!-- Inner Card 2 -->
 
-    <v-row>
-    <v-col 
-        md="6">
-      <v-card 
-      class="box3 my-12"
-      width="90%"
-      height="300px"
-      color="#ffffff4d"
-    >
+    <!-- <v-row> <v-col 
+        md="6"> -->
+            <v-card
+            class="box3 my-12" 
+            width="96%"
+            height="300px"
+            color="#ffffff4d"
+            >
 
-    <v-card-title>In Progress</v-card-title>
+            <v-card-title>Task List</v-card-title>
+
+            <!-- <ul class="task-list">
+                <li class="task-list-item">
+                  <label class="task-list-item-label">
+                    <input type="checkbox">
+                    <span>Task</span>
+                  </label>
+                  <span class="delete-btn" title="Delete Task"></span>
+                </li>
+             </ul> -->
+
+             <table class="table table-striped">
+                <thead>
+                    <tr>
+                    <th scope="col">Task</th>
+                    <th scope="col">Status</th>
+                    <th scope="col" class="text-center">#</th>
+                    <th scope="col" class="text-center">#</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(task, index) in tasks" :key="index">
+                    <th>{{ task.name }}</th>
+                    <td>{{ task.status }}</td>
+                    <td>
+                        <div class="text-center">
+                            <span class="fa fa-pen"></span>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="text-center">
+                            <span class="fa fa-trash"></span>
+                        </div>
+                    </td>
+                    </tr>
+                </tbody>
+                </table>
+      
+        
+          
+          
+        
+
+
     
-    </v-card>
-</v-col>
+            </v-card>
+<!-- </v-col> -->
 
         
-         <!-- Inner Card 3 -->
+         <!-- Inner Card 3
     <v-col 
         md="6">
       <v-card 
@@ -60,25 +220,121 @@
     <v-card-title>Done</v-card-title>
         </v-card>
     </v-col>
-    </v-row>
+    </v-row> -->
     
-
-
     </v-card>
 
     
-</v-col>
-</v-row>
+        </v-col>
+   </v-row>
 </v-container>
 </template>
 
 <script>
-export default {
+// import CreatePop from './components/CreatePop.vue'
 
+export default {
+//     name: 'MainPage',
+//   components: {
+//     CreatePop
+//   },
+
+    data() {
+        return {
+
+            //dialog box
+            dialog: false,
+
+            // Form Validations
+                name: '',
+            nameRules: [
+                v => !!v || 'Name is required',
+                v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+            ],
+
+            aname: '',
+            anameRules: [
+                v => !!v || 'Name is required',
+                v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+            ],
+
+            date: '',
+            dateRules: [
+                v => !!v || 'Due date is required',
+                v => (v && v.length <= 10) || 'Date must be in the Format dd/mm/yyyy',
+            ],
+
+            email: '',
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            ],
+
+            sum: '',
+            sumRules: [
+                v => !!v || 'Summary is required',
+                // v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            ],
+
+            des: '',
+            desRules: [
+                v => !!v || 'Description is required',
+                // v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            ],
+
+            
+            
+
+            //TaskList
+            task: '',
+            tasks: [
+                {
+                   name: 'Steal bananas from the store.',
+                   status: 'to-do'
+                },
+                {
+                    name: 'Eat 1kg chocolate in 1 hour.',
+                    status: 'in-progress'
+                }
+            ]
+        }
+    },
+
+    methods: {
+        submitTask(){
+            console.log('Hello submit task')
+        }
+    }
+
+
+    // el: '#tasklist',
+    // data: {
+    //     title: 'to do list'
+    //     tasks: [
+    //         {name: 'Today : Internal Meeting'}
+    //         {name: 'Tomorrow : Read a book'}
+    //         {name: 'Daily : Event Joins'}
+    //     ]
+    // },
+
+    // methods: {
+    //     newItem: function() {
+    //         if (!this.tasks.name){
+    //             retuen
+    //         }
+    //         this
+    //     }
+    // }
 }
 </script>
 
 <style>
+
+.btt {
+    margin-right: 20px;
+    margin-top: 15px;
+    
+}
 
 .box1 {
     margin-left: 20px;
@@ -86,14 +342,13 @@ export default {
     
 }
 
-.box2 {
+/* .box2 {
     margin-left: 0%;
     margin-right: 20px;
-}
+} */
 
 .box3 {
     margin-left: 20px;
-    margin-right: 52%;
     
 }
 
@@ -102,6 +357,7 @@ body {
     height: 100vh;
     margin: 0;
     padding: 15px;
+    
     
     justify-content: center;
     align-items: center;
@@ -160,5 +416,71 @@ body {
     color: #2e3136;
   }
 
+  .task-list-item {
+    background-color: rgba(255, 255, 255, 0.7);
+    display: flex;
+    align-items: center;
+    padding: 8px;
+    border-radius: 35px;
+    margin-bottom: 12px;
+    margin-right: 20px;
+    margin-left: 10px;
+  }
 
+  .task-list-item input {
+    width: 16px;
+    height: 16px;
+    border: 1px solid #272727;
+    border-radius: 50%;
+    background-image: url(circle.png);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 0;
+    transition: 0.2s;
+    margin-right: 8px;
+    flex-shrink: 0;
+    margin-top: 4px;
+    appearance: none;
+  }
+
+  .task-list-item input:hover {
+    border-color: var(--checkbox-color);
+    box-shadow: 0 0 0 3px var(--checkbox-shadow);
+  }
+
+  .task-list-item input:checked {
+    background-size: 10px;
+    border: 1px solid var(--checkbox-color);
+    background-color: var(--checkbox-color);
+  }
+
+  .task-list-item input:checked + span {
+    color: #272727;
+    text-decoration: line-through #272727;
+  }
+
+  .task-list-item-label {
+    display: flex;
+    align-items: flex-start;
+    color: #272727;
+    margin-right: 8px;
+    font-size: 14px;
+    line-height: 24px;
+    position: relative;
+    transform: 0.2s;
+    cursor: pointer;
+  }
+
+  .delete-btn {
+    margin-left: auto;
+    margin-right: 5px;
+    display: block;
+    width: 16px;
+    height: 16px;
+    background-image: url(trash.png);
+    background-repeat: no-repeat;
+    background-size: 16px;
+    background-position: center;
+    cursor: pointer;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import { createApp } from 'vue'
 import MainPage from './components/MainPage.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 loadFonts()
 
